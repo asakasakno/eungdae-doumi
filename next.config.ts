4ad77d4331e91f2ace035fd70646aab3484ac545
asakasakno <qowlgns02548@gmail.com>
2026-03-21 01:11:40 +0900

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
